2e8bd83cd81f8632d2047d47fbc48136421151ec
Devon Quinn <devon.q.traver@gmail.com>
2024-01-31 19:25:15 -0500

    updated asset bundling to include `@sounds`

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,28 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import path from 'path';
+import copy from 'rollup-plugin-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@fonts': path.resolve(__dirname, './src/fonts')
+      '@fonts': path.resolve(__dirname, './src/font'),
+      '@sounds': path.resolve(__dirname, './src/assets/sounds')
     },
   },
-  plugins: [preact()]
+  plugins: [
+    preact(),
+    copy({flatten: false,
+      targets: [
+        { src: "src/**/*.(html|svg|png|jpg|gif|json|mp3|eot|ttf|woff)", dest: "dist" }
+      ],
+    })
+  ],
+  build: {
+    emptyOutDir: false,
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
