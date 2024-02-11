dcd4b13ac036840d8d1fe8ef372e68282a7417c6
Devon Quinn <devon.q.traver@gmail.com>
2024-02-11 00:35:10 -0500

    Update ActionToast.tsx

--- src/components/ActionToast.tsx
+++ src/components/ActionToast.tsx
@@ -1,28 +1,9 @@
 import {ActionType, BaseToastDelay, GameAction } from "../TetrisConfig";
-import {animate, motion} from "framer-motion";
-// import BackToBack from "./BackToBack";
+import {motion} from "framer-motion";
 import TSpin from "../TSpin";
-// import { pseudoRandom } from "../utils/AppUtil";
 import BackToBack from "./BackToBack";
-// import { useEffect } from "preact/hooks";
-
 
 const LABEL_COMBO: string = "COMBO";
-
-const easeOutBounce = (x: number): number => {
-  const n1 = 7.5625;
-  const d1 = 2.75;
-  
-  if (x < 1 / d1) {
-      return n1 * x * x;
-  } else if (x < 2 / d1) {
-      return n1 * (x -= 1.5 / d1) * x + 0.75;
-  } else if (x < 2.5 / d1) {
-      return n1 * (x -= 2.25 / d1) * x + 0.9375;
-  } else {
-      return n1 * (x -= 2.625 / d1) * x + 0.984375;
-  }
-};
 
 const transitionEnd = {
   opacity: 0,
