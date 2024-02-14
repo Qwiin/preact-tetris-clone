aa7d3a59138c852112fc2a9509e4b174af0abaed
Devon Quinn <devon.q.traver@gmail.com>
2024-02-13 23:50:58 -0500

    revert pause screen change.

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1558,7 +1558,7 @@ const Game = (props: GameProps) => {
                 {renderBoard()}
               </div>
               { (gameover || paused.current) &&
-                <div className="tw-flex tw-items-center tw-justify-center tw-absolute tw-w-40 tw-h-80  tw-z-10 tw-top-0 tw-left-0">
+                <div className="tw-flex tw-items-center tw-justify-center tw-absolute tw-w-40 tw-h-80 tw-bg-black tw-bg-opacity-40 tw-z-10 tw-top-0 tw-left-0">
                   <h2 className="tw-text-center tetris-font tw-text-lg">{gameover ? 'Game Over' : 'Paused'}</h2>
                 </div>
               }
